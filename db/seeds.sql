e93ac54d4646a59679c671c18987b82e4b2a692c
jcpax <jcpax5@gmail.com>
2016-11-19 15:48:15 -0600

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Bison Burger');
+INSERT INTO burgers (burger_name) VALUES ('Spicy Pepper Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Original Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Deep Fried Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Poblano Mushroom Burger');
